d69aac450c4bae18eaffbe3af01627e6bc3752cb
Ar Alfin Arif <cleavercode@gmail.com>
2021-03-11 21:50:29 +0300

    django and react integration

---
+++ my-app/src/components/MyComponent.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+class MyComponent extends Component{
+
+
+    render(){
+        return(
+            <div>
+                <h1>Hey Alfin! This is my component</h1>
+            </div>
+        )
+    }
+
+}
+
+export default MyComponent;
